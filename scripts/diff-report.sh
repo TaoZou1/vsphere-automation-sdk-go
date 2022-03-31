97595b5499a9223ef6b5a7d87bd0204ffbf27a66
aagrawal3 <aagrawal3@vmware.com>
2022-03-31 17:14:07 +0530

    Suggest next version.

--- scripts/diff-report.sh
+++ scripts/diff-report.sh
@@ -7,42 +7,58 @@ echo "Module path: $modulePath"
 
 mkdir /workspace/go-sdk-main
 mkdir /workspace/go-sdk-tag
-
-cd /workspace/go-sdk-main
+mkdir -p /workspace/results/$modulePath
 
 # Clone repo
+cd /workspace/go-sdk-main
 git clone https://oauth2:${SDK_CLONE_ACCESS_TOKEN}@gitlab.eng.vmware.com/vapi-sdk/vsphere-automation-sdk-go.git
-
 echo "Copy repo in tag dir..."
 cp -R /workspace/go-sdk-main/vsphere-automation-sdk-go /workspace/go-sdk-tag/
 
+# Checkout respective branch codes
 cd /workspace/go-sdk-main/vsphere-automation-sdk-go/
-git checkout main
-git pull origin main
-
+git checkout aagrawal3/main/automate-sementic-versioning
+git pull origin aagrawal3/main/automate-sementic-versioning
 cd /workspace/go-sdk-tag/vsphere-automation-sdk-go/
-# Get new tags from remote
 git fetch --tags
-
-# Get latest tag name
 latestTag=$(git describe --match "$modulePath*" --tags `git rev-list --tags --max-count=1`)
 echo "Last tagged version for $modulePath was $latestTag"
-
-# Identify the service
-# modulePath=$(echo $latestTag | rev | cut -d "/" -f 2- | rev)
-mkdir -p /workspace/results/$modulePath
-ls -la /workspace/results/$modulePath
-
-# Checkout latest tag
 git checkout $latestTag
 
 # run diff check
 cd /workspace/diff-checker
-echo "Old mod files"
-ls -la /workspace/go-sdk-tag/vsphere-automation-sdk-go/$modulePath
-echo "New mod files"
-ls -la /workspace/go-sdk-main/vsphere-automation-sdk-go/$modulePath
-
 echo "Generating diff..."
 go run cmd/module-diff-check.go generate-report --o /workspace/go-sdk-tag/vsphere-automation-sdk-go/$modulePath --n /workspace/go-sdk-main/vsphere-automation-sdk-go/$modulePath --result-dir /workspace/results/$modulePath --lang go
-cat /workspace/results/$modulePath/go-mod-final-report.json
+
+# find release type
+RELEASE_TYPE=cat /workspace/results/$modulePath/go-mod-final-report.json | jq '.ReleaseType'
+echo "Detected release type: $RELEASE_TYPE"
+
+# Update version file in main branch...
+cd /workspace/go-sdk-main/vsphere-automation-sdk-go/
+lastReleaseVersion="${latestTag##*/}"
+versionArray=($(echo $lastReleaseVersion | tr '.' "\n"))
+nextRelease=lastReleaseVersion
+
+if [[ $RELEASE_TYPE  ==  "MAJOR" ]]
+then
+  # remove 'v' from beginning
+  majorVersion=${versionArray[1]:1}
+  majorVersion=$(( majorVersion + 1 ))
+  nextRelease="v$majorVersion.$versionArray[2].$versionArray[3]"
+  echo "Next release version: $nextRelease"
+elif [[ $RELEASE_TYPE  ==  "MINOR" ]]
+then
+  minorVersion=$versionArray[2]
+  minorVersion=$(( minorVersion + 1 ))
+  nextRelease="$versionArray[1].$minorVersion.$versionArray[3]"
+  echo "Next release version: $nextRelease"
+elif [[ $RELEASE_TYPE  ==  "PATCH" ]]
+then
+  patchVersion=$versionArray[3]
+  patchVersion=$(( patchVersion + 1 ))
+  nextRelease="$versionArray[1].$versionArray[2].$patchVersion"
+  echo "Next release version: $nextRelease"
+else
+  echo "no change detected..."
+fi
