bef2704c2393e58efa0076513271cf1c9b3ef4ab
aagrawal3 <aagrawal3@vmware.com>
2022-04-28 16:21:18 +0530

    Test sync to github.

--- scripts/sync_to_github.sh
+++ scripts/sync_to_github.sh
@@ -1,4 +1,17 @@
 #! /bin/bash
 set -e
 
-# TODO logic to sync release branch to github.
+mkdir /workspace/go-sdk-release
+
+# Copy sources
+cd /workspace/go-sdk-release
+cp -R /builds/vapi-sdk/vsphere-automation-sdk-go ./
+cd vsphere-automation-sdk-go
+git remote set-url origin https://oauth2:${GO_SDK_PROJECT_ACCESS_TOKEN}@gitlab.eng.vmware.com/vapi-sdk/vsphere-automation-sdk-go.git
+git remote add github https://oauth2:${GITHUB_SDK_CODE_SYNC_CI_TOKEN}@github.com/vmware/vsphere-automation-sdk-go.git
+
+# Checkout and push
+cd /workspace/go-sdk-release/vsphere-automation-sdk-go/
+git checkout release
+git pull origin release
+git push --follow-tags github HEAD:aagrawal3/test/release-sync
